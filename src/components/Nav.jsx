797efaf191ceced23dd5fabaab3ad44db498c8a9
mostafa <mostafameerzad@gmail.com>
2024-12-20 17:03:14 +0430

    implement navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,22 +4,27 @@ import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
+    <header className="padding-x py-8 w-full absolute z-10">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
-          <img src={headerLogo} alt="LOGO" height={29} width={130} />
+          <img src={headerLogo} alt="logo" height={20} width={120} />
         </a>
-
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-        {
-          navLinks.map((link) => (
-           <li key={link.label}>
-            <a href={link.href} className="font-montserrat leading-normal text-lg text-slate-gray">{link.label}</a>
-           </li> 
-          ))
-        }
+        <ul className="flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-sm text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
-        <img src={hamburger} alt="hamburger" width={25} height={25} className="hidden max-lg:block"/>
+
+        <div className="hidden max-lg:block ">
+          <img src={hamburger} alt="Hamburger" width={18} height={18} />
+        </div>
       </nav>
     </header>
   );
