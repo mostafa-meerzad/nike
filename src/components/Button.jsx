ea3275a77ab552d2e0298f27110255c7cd34f726
mostafa <mostafameerzad@gmail.com>
2025-01-19 20:41:31 +0430

    Complete SuperQuality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ const Button = ({ label, iconUrl }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-6 py-3 border font-montserrat text-lg leading-none bg-coral-red rounded-full border-coral-red text-white">
       {label}
-      <img
-        src={iconUrl}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
