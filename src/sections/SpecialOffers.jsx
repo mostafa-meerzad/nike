04f725ea17021ef5bd6ab210416e907ab602ab6a
mostafa <mostafameerzad@gmail.com>
2025-01-20 09:28:25 +0430

    Complete Customer Review section

--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -5,7 +5,6 @@ import Button from "../components/Button";
 const SpecialOffers = () => {
   return (
     <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
-      \
       <div className="flex-1">
         <img
           src={offer}
