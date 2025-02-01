1be702be337003a66edbce5cf03df42f07681a02
mostafa <mostafameerzad@gmail.com>
2025-02-01 16:28:09 +0430

    Complete adding animation to services section

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import { motion } from "motion/react";
 
-const ServiceCard = ({ imgURL, label, subtext }) => {
+const ServiceCard = ({ imgURL, label, subtext, variant }) => {
   return (
-    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
+    <motion.div
+      variants={variant}
+      className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16 "
+    >
       <div className="w-11 h-11 justify-center items-center flex rounded-full bg-coral-red">
         <img src={imgURL} alt={label} width={24} height={24} />
       </div>
@@ -12,7 +15,7 @@ const ServiceCard = ({ imgURL, label, subtext }) => {
       <p className="mt-3 break-words font-montserrat text-md leading-normal text-slate-gray">
         {subtext}
       </p>
-    </div>
+    </motion.div>
   );
 };
 
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,13 +1,35 @@
+import { motion } from "motion/react";
 import ServiceCard from "../components/ServiceCard";
 import { services } from "../constants";
 
 const Services = () => {
+  const parentVariant = {
+    visible: {
+      transition: {
+        staggerChildren: 0.5,
+      },
+    },
+    hidden: {},
+  };
+  const childVariant = {
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    hidden: {
+      opacity: 0,
+      y: 10,
+    },
+  };
+
   return (
-    <section className="max-container flex justify-center flex-wrap gap-9">
+    <motion.section
+      initial={"hidden"}
+      whileInView={"visible"}
+      variants={parentVariant}
+      className="max-container flex justify-center flex-wrap gap-9 "
+    >
       {services.map((service) => (
-        <ServiceCard key={service.label} {...service} />
+        <ServiceCard variant={childVariant} key={service.label} {...service} />
       ))}
-    </section>
+    </motion.section>
   );
 };
 export default Services;
